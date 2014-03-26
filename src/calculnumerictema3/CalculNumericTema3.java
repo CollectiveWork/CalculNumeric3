a60a67c5c3c7b78a70698e529c07c87be3f24bc5
Gabriel <gabrielbudau@gmail.com>
2014-03-26 16:36:23 +0200

    AM finalizat metoga Gauss-Seidel si am adaugat metoda gradientului conjugat, dar cea din urma trebuie revizuita.

--- src/calculnumerictema3/CalculNumericTema3.java
+++ src/calculnumerictema3/CalculNumericTema3.java
@@ -16,8 +16,8 @@ public class CalculNumericTema3 {
      Se va rula pentru matricile de marimea n=20, 50, 100, iar epsilon = 10^(-10)
      */
 
-    final String DOUBLE_DISPLAY_FORMAT = "%+2g";
-    final String RESULT_DISPLAY_FORMAT = "%+10g";;
+    final String DOUBLE_DISPLAY_FORMAT = "%+g";
+    final String RESULT_DISPLAY_FORMAT = "%+15g";
     final Integer SYST_SIZE = 4;
     final double epsilon = 10e-10;
 
@@ -41,7 +41,6 @@ public class CalculNumericTema3 {
         } else {
             return 0;
         }
-
     }
 
     protected double getBelement(int i) {
@@ -107,7 +106,7 @@ public class CalculNumericTema3 {
                 temp_b[i] = sigma * b[i];
             }
             double er = 0.0;
-            
+
             do {
                 double y[] = new double[n];
 
@@ -123,111 +122,177 @@ public class CalculNumericTema3 {
                 for (int i = 1; i < n; i++) {
                     double suma = 0.0;
                     for (int j = 1; j < n; j++) {
-                        suma += A[i][j]*(y[i] - x_JR[i])*(y[j] - x_JR[j]);
+                        suma += A[i][j] * (y[i] - x_JR[i]) * (y[j] - x_JR[j]);
                     }
                     norma_suma += suma;
 
                 }
                 er = norma_suma;
-                for(int i = 1; i < n ; i++)
+                for (int i = 1; i < n; i++) {
                     x_JR[i] = y[i];
-                
+                }
+
                 q++;
-                
+
             } while (er >= epsilon);
             //System.out.println("N: " + n);
-            
+
         }
-         System.out.printf("\n%30s (", "METODA JACOBI RELAXATA");
-            for (int j = 1; j < n; j++) {
-                System.out.printf(" " + RESULT_DISPLAY_FORMAT + " ", x_JR[j]);
-            }
-            System.out.printf(")");
+        System.out.printf("\n%30s (", "METODA JACOBI RELAXATA");
+        for (int j = 1; j < n; j++) {
+            System.out.printf(" " + RESULT_DISPLAY_FORMAT + " ", x_JR[j]);
+        }
+        System.out.printf(")");
     }
 
-    protected void GS(int iteratii){
+    protected void GS(int iteratii) {
+        double sigma = 0.0, sigmao = 0.0;
+        double xo[] = new double[n];
+        double y[] = new double[n];
+        double xi[][] = new double[n][n];
         int p = iteratii;
-        double sigma = 0.0;
-        int nn;
-        for (int k = 1; k < p-1; k ++) {
-            sigma = (2*k)/p;
-           
-            double er = 0.0;
-            double x_n[] = new double[n];
-            double x_n_1[] = new double[n];
-            for(int i = 1; i < n; i ++){
-                //x_GS[i] = 0.0;
-                x_n[i] = 0.0;
-                x_n_1[i] = 0.0;
-            }
-            nn = 0;
-            
-            do{
-                
-                
-                for(int i = 1; i < n; i ++){
-                    if(i == 1){
-                        double suma = 0.0;
-                        
-                        for(int j = 1; j < n; j++)
-                            suma += A[i][j]*x_n[j];
-                        x_n_1[i] = (1-sigma)*x_GS[i] - ((sigma/A[i][i])*(b[i] - suma));
-                    }else{
-                        double suma1 = 0.0;
-                        double suma2 = 0.0;
-                        for (int j = 1; j <= i-1; j++) {
-                            suma1 += A[i][j] * x_n_1[j];
-                        }
-                        for (int j = i + 1; j <n; j++) {
-                            suma1 += A[i][j] * x_n[j];
-                        }
-                         x_n_1[i] = (1 - sigma) * x_n[i] - ((sigma/A[i][i])*(b[i] - suma1 - suma2));
-                    }
-                        
-                }
-                
-                double y[] = new double[n];
-                for(int i = 1; i < n ; i++)
-                    y[i] = x_n_1[i];
-                
-                double norma_suma = 0.0;
-                for (int i = 1; i < n; i++) {
-                    double suma = 0.0;
-                    for (int j = 1; j < n; j++) {
-                        suma += A[i][j]*(y[i] - x_n_1[i])*(y[j] - x_n_1[j]);
-                    }
-                    norma_suma += suma;
-
-                }
-                er = norma_suma;
-                for(int i = 1; i < n ; i++)
-                    x_GS[i] = y[i];
-                
-                nn++;
-               for(int i = 1; i < n ; i++)
-                    x_n[i] = x_n_1[i];
-                
-            }while (er >= epsilon);
-            
+        int m, mo = 0;
+        double suma = 0.0, suma2 = 0.0, temp = 0.0, er = 0.0;
+        for (int i = 1; i < n; i++) {
+            x[i] = 0;
+            xo[i] = 0;
+            y[i] = 0;
+            xi[0][i] = 0;
         }
-         System.out.printf("\n%30s (", "METODA GAUSS-SEIDEL RELAXATA");
-            for (int j = 1; j < n; j++) {
-                System.out.printf(" " + RESULT_DISPLAY_FORMAT + " ", x_GS[j]);
+        for (int k = 1; k <= p - 1; k++) {
+            sigma = 2 * k;
+            sigma = sigma / p;
+            m = 0;
+            do {
+                y[1] = (1 - sigma) * x[1];
+                suma = 0;
+                for (int j = 2; j < n; j++) {
+                    suma = suma + A[1][j] * x[j];
+                }
+                temp = sigma / A[1][1];
+                y[1] = y[1] + temp * (b[1] - suma);
+                for (int i = 2; i < n; i++) {
+                    y[i] = (1 - sigma) * x[i];
+                    suma = 0;
+                    for (int j = 1; j <= i - 1; j++) {
+                        suma = suma + A[i][j] * y[j];
+                    }
+                    suma2 = 0;
+                    for (int j = i + 1; j < n; j++) {
+                        suma2 = suma2 + A[i][j] * x[j];
+                    }
+                    temp = (double) (sigma / A[i][i]);
+                    y[i] = y[i] + temp * (b[i] - suma - suma2);
+                }
+                suma = 0;
+                for (int i = 1; i < n; i++) {
+                    for (int j = 1; j < n; j++) {
+                        suma = suma + A[i][j] * (y[i] - x[i]) * (y[j] - x[j]);
+                    }
+                }
+                er = (sqrt(suma));
+                for (int i = 1; i < n; i++) {
+                    x[i] = y[i];
+                }
+                m++;
+            } while (er >= epsilon);
+            if (k == 1) {
+                mo = m;
+                sigmao = sigma;
+                for (int i = 1; i < n; i++) {
+                    xo[i] = x[i];
+                }
+            } else if (m < mo) {
+                mo = m;
+                sigmao = sigma;
+                for (int i = 1; i < n; i++) {
+                    xo[i] = x[i];
+                }
             }
-            System.out.printf(")");
+        }
+        System.out.printf("\n%30s (", "METODA GAUSS-SEIDEL RELAXATA");
+        for (int j = 1; j < n; j++) {
+            System.out.printf(" " + RESULT_DISPLAY_FORMAT + " ", xo[j]);
+        }
+        System.out.printf(") n0 = " + mo + " | sigma = " + sigmao);
+
     }
-    
-    protected void GC(int iteratii){
-        
+
+    protected void GC() {
+        double alfa[] = new double[n];
+        double ci[] = new double[n];
+        double xi[][] = new double[n][n];
+        double ri[][] = new double[n][n];
+        double vi[][] = new double[n][n];
+        double suma = 0.0, suma2 = 0.0, suma3 = 0.0, er = 0.0;
+
+        for (int i = 1; i < n; i++) {
+            suma = 0;
+            for (int j = 1; j < n; j++) {
+                suma = suma + A[i][j] * xi[0][i];
+            }
+            ri[0][i] = b[i] - suma;
+            vi[0][i] = ri[0][i];
+            xi[0][i] = 0;
+        }
+
+        int k = 0;
+        do {
+            suma = 0;
+            for (int j = 1; j < n; j++) {
+                suma += ri[k][j] * ri[k][j];
+            }
+            suma2 = 0;
+            for (int i = 1; i < n; i++) {
+                suma3 = 0;
+                for (int j = 1; j < n; j++) {
+                    suma3 = suma3 + A[i][j] * vi[k][j];
+                }
+                suma2 += suma3 * vi[k][i];
+            }
+            alfa[k] = (suma / suma2);
+            for (int i = 1; i < n; i++) {
+                xi[k + 1][i] = xi[k][i] + alfa[k] * vi[k][i];
+            }
+            suma = 0;
+            for (int j = 1; j < n; j++) {
+                suma += (xi[k + 1][j] - xi[k][j]) * (xi[k + 1][j] - xi[k][j]);
+            }
+            er = (sqrt(suma));
+            for (int i = 1; i < n; i++) {
+                suma = 0;
+                for (int j = 1; j < n; j++) {
+                    suma += A[i][j] * xi[k + 1][j];
+                }
+                ri[k + 1][i] = b[i] - suma;
+            }
+            suma = 0;
+            suma2 = 0;
+            for (int j = 1; j < n; j++) {
+                suma += ri[k + 1][j] * ri[k + 1][j];
+                suma2 += ri[k][j] * ri[k][j];
+            }
+            ci[k] = suma / suma2;
+            for (int i = 1; i < n; i++) {
+                vi[k + 1][i] = ri[k + 1][i] + ci[k] * vi[k][i];
+            }
+
+        } while (er >= epsilon && k++ < n-2);
+
+        System.out.printf("\n%30s (", "METODA GRADIENTULUI GONJUGAT");
+        for (int j = 1; j < n; j++) {
+            System.out.printf(" " + RESULT_DISPLAY_FORMAT + " ", xi[k - 1][j]);
+        }
+        System.out.printf(") ");
     }
-    
-    
+
     public static void main(String[] args) {
         CalculNumericTema3 cnt3 = new CalculNumericTema3();
         cnt3.initArrays();
         cnt3.printSystem(cnt3.x);
         cnt3.JR(cnt3.SYST_SIZE * 1000);
-        cnt3.GS(cnt3.SYST_SIZE * 1);
+        cnt3.GS(cnt3.SYST_SIZE * 1000);
+        cnt3.GC();
         System.out.println("");
     }
 }
